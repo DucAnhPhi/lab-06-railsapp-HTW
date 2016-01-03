11cf8a80fc793a6e8ba5701ae4a06c2e5e83c7e8
DucAnhPhi <ducphi1@yahoo.de>
2016-01-03 22:57:10 +0100

    revert changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
-    @customer = Customer.find(params[:id])
+    
   end
 
   # GET /orders/new
