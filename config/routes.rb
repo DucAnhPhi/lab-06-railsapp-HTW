3687462aebd666105e31a0f53bbf164de6aa39ed
DucAnhPhi <ducphi1@yahoo.de>
2016-01-28 17:58:42 +0100

    added I18n functionality and de.yml file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  scope ":locale" do
   resources :orders
 
   resources :customers
 
   resources :pumpkins
+
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
